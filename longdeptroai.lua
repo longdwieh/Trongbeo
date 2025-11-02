414ecb5b842462025a861e6bfe12dd371fac07bb
longdwieh <longhyaz2012@gmail.com>
2025-11-02 12:27:57 +0700

    Update longdeptroai.lua

--- longdeptroai.lua
+++ longdeptroai.lua
@@ -6,16 +6,7 @@ local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/dauco
     SaveFolder = "Blox Fruits.lua"
 })
 Windows:AddMinimizeButton({
-  Button = { Image = "rbxassetid://131151731604309", BackgroundTransparency = 0 },
-  Corner = { CornerRadius = UDim.new(0, 6) }
+  Button = { Image = "rbxassetid://111367887827717", BackgroundTransparency = 0 },
+  Corner = { CornerRadius = UDim.new(0, 4) }
 })
 
-local Tabs = {
-Main = Window:AddTab({Title = "Tab Farming", Icon = "Home"}),
-Settings = Window:AddTab({Title = "Tab Local player", Icon = "setting"}),
-Prehistoric = Window:AddTab({Title = "Tab Prehistoric Event", Icon = "tent"}),
-SeaEvent = Window:AddTab({Title = "Tab Sea Event", Icon = "anchor"}),
-Dragon = Window:AddTab({Title = "Tab Dragon Dojo", Icon = "layers"}),
-Mirage = Window:AddTab({Title = "Tab Race V4", Icon = "flag"}),
-Melee = Window:AddTab({Title = "Tab Get Item", Icon = "loader"}),
-Quests = Window:AddTab({Title = Tab Farming Oder", Icon = "sword"}),
