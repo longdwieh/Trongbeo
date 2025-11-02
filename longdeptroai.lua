49c081c9e793f3751ff5ad4cfc68413d4b2789ec
longdwieh <longhyaz2012@gmail.com>
2025-11-02 12:26:06 +0700

    Update longdeptroai.lua

--- longdeptroai.lua
+++ longdeptroai.lua
@@ -10,4 +10,12 @@ Windows:AddMinimizeButton({
   Corner = { CornerRadius = UDim.new(0, 6) }
 })
 
-local Farm = Windows:MakeTab({"Discord","Info"})
+local Tabs = {
+Main = Window:AddTab({Title = "Tab Farming", Icon = "Home"}),
+Settings = Window:AddTab({Title = "Tab Local player", Icon = "setting"}),
+Prehistoric = Window:AddTab({Title = "Tab Prehistoric Event", Icon = "tent"}),
+SeaEvent = Window:AddTab({Title = "Tab Sea Event", Icon = "anchor"}),
+Dragon = Window:AddTab({Title = "Tab Dragon Dojo", Icon = "layers"}),
+Mirage = Window:AddTab({Title = "Tab Race V4", Icon = "flag"}),
+Melee = Window:AddTab({Title = "Tab Get Item", Icon = "loader"}),
+Quests = Window:AddTab({Title = Tab Farming Oder", Icon = "sword"}),
